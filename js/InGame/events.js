9ad00ac6ab6f72610fc5075f8331a7d15c45e57b
Dwight Huanca <dwightht.anthony955@gmail.com>
2023-01-13 22:44:29 -0500

    Solucion del bug de las vidas

--- js/InGame/events.js
+++ js/InGame/events.js
@@ -1,29 +1,35 @@
 // Se genera el numero misterioso entre el rango 40 a 100
 let inputNumber = 0;
+let maxDigit = 3;
 /*INPUT NUMBER*/ 
 // Cuando se empieza a escrbir en el input de texto
-input.addEventListener("keyup", () => {
+
+/* oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" */
+input.addEventListener('input', showNumberString);
+function showNumberString(){
     inputNumber = input.value;
-    textBigNumber.textContent= inputNumber ? inputNumber : '?';
-});
+    if(inputNumber.length > maxDigit){
+        input.value = inputNumber.slice(0, maxDigit);
+    }
+    textBigNumber.textContent = inputNumber ? input.value : '?';
+}
 // click al boton GO
 btnGo.addEventListener('click', function(){
-    // Obtener el valor del numero ingresado
-    inputNumber = parseInt(input.value);
-
-    if(numberMysterious == inputNumber){
-        let actuallyDifficulty = getDataActuallyGame().difficulty.value;
-        let newDifficulty = actuallyDifficulty < 2 ? actuallyDifficulty + 1 : actuallyDifficulty;
-        pointsAccumulator();
-        assignDifficulty(newDifficulty);
-    } else {
-        // Actualizacion de la vida
-        lifeActually(countLifes - 1); 
-        
-        if (countLifes == 0){
-            saveDataEndGame();
-        }
-        // verificar la distancia de cercania
-        check_distance(numberMysterious, inputNumber);
-    } 
+    if(input.value.length != 0){
+        // Obtener el valor del numero ingresado
+        inputNumber = parseInt(input.value);
+        if(countLifes != 0){
+            if(numberMysterious == inputNumber){
+                let actuallyDifficulty = getDataActuallyGame().difficulty.value;
+                let newDifficulty = actuallyDifficulty < 2 ? actuallyDifficulty + 1 : actuallyDifficulty;
+                pointsAccumulator();
+                assignDifficulty(newDifficulty);
+            } else {
+                // Actualizacion de la vida
+                lifeActually(countLifes - 1); 
+                // verificar la distancia de cercania
+                check_distance(numberMysterious, inputNumber);
+            } 
+        }else{saveDataEndGame();}
+    } else{result.innerText = "Write a number"}
 })
